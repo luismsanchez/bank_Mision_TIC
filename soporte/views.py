a5e16dda7891316f07320e2e99ea8fcd09641fcf
luismsanchez <luism.sanchez13@gmail.com>
2021-11-10 19:59:27 -0500

    New Views

--- soporte/views.py
+++ soporte/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import PersonaSoporte, PQR
+from .serializers import PersonaSoporteSerializer, PQRSerializer
+
 
 # Create your views here.
+
+class PersonaSoporteListCreate(generics.ListCreateAPIView):
+    queryset = PersonaSoporte.objects.all()
+    serializer_class = PersonaSoporteSerializer
+
+class PersonaSoporteUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PersonaSoporte.objects.all()
+    serializer_class = PersonaSoporteSerializer
+
+
+class PQRListCreate(generics.ListCreateAPIView):
+    queryset = PQR.objects.all()
+    serializer_class = PQRSerializer
+
+class PQRUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PQR.objects.all()
+    serializer_class = PQRSerializer
